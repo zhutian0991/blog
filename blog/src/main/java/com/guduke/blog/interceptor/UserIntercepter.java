db9ba4292068665089e440b42bca894799c872e2
guduke <1287286145@qq.com>
2017-09-09 12:13:08 +0800

    添加用户拦截器

---
+++ blog/src/main/java/com/guduke/blog/interceptor/UserIntercepter.java
@@ -0,0 +1,37 @@
+package com.guduke.blog.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.guduke.blog.entity.User;
+
+public class UserIntercepter implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		User user = (User) request.getSession().getAttribute("suser");
+		//session获取不到user数据时，重定向到首页
+		if(user==null){
+			response.sendRedirect("/blog");
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+
+	}
+
+}
