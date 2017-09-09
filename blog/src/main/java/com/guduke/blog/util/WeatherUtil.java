c6d0e38d1f8562327667550703a159e9d632a55f
guduke <1287286145@qq.com>
2017-09-09 12:14:27 +0800

    完善天气调用工具类

--- blog/src/main/java/com/guduke/blog/util/WeatherUtil.java
+++ blog/src/main/java/com/guduke/blog/util/WeatherUtil.java
@@ -14,11 +14,15 @@ public class WeatherUtil {
 		WeatherWebServiceSoap webServiceSoap = factory.getWeatherWebServiceSoap();
 		ArrayOfString arrayOfString = webServiceSoap.getWeatherbyCityName(city);
 		List<String> list = arrayOfString.getString();
-		String emperature = list.get(5);
 		String tq = list.get(6).substring(5);
-		String pic1 = list.get(8);
-		String pic2 = list.get(9);
-		Weather weather = new Weather(city, emperature, tq, pic1, pic2);
-		return weather;
+		if(tq.equals("域暂时不被支持。http://www.webxml.com.cn/")){
+			return null;
+		}else{
+			String emperature = list.get(5);
+			String pic1 = list.get(8);
+			String pic2 = list.get(9);
+			Weather weather = new Weather(city, emperature, tq, pic1, pic2);
+			return weather;
+		}
 	}
 }
